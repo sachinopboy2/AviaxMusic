01457542076d29ba1daa45fff2374cc101fbfaa7
sachinopboy2 <sachin25u7@gmail.com>
2025-08-16 13:40:46 +0530

    Update database.py

--- AviaxMusic/utils/database.py
+++ AviaxMusic/utils/database.py
@@ -3,8 +3,8 @@ import asyncio
 from datetime import date
 from typing import Dict, List, Union
 
-from AnonMusic import userbot
-from AnonMusic.core.mongo import mongodb
+from AviaxMusic import userbot
+from AviaxMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -93,7 +93,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AnonMusic.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -107,7 +107,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AnonMusic.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -134,7 +134,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AnonMusic.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -147,7 +147,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AnonMusic.core.userbot import assistants
+    from AviaxMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -687,3 +687,4 @@ async def remove_banned_user(user_id: int):
     if not is_gbanned:
         return
     return await blockeddb.delete_one({"user_id": user_id})
+
